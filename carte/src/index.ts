d51ac48fe6823bf3b8a8c462f8a252a9ba503eda
avaice <dev@cho-ice.xyz>
2025-08-10 03:35:19 +0900

    carte: healthCheckを実装

--- carte/src/index.ts
+++ carte/src/index.ts
@@ -1,11 +1,22 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import healthCheck from "./healthCheck";
 
 var app = new Hono();
+
+app.onError((error, c) => {
+	return c.json({ error: error.message }, 500);
+});
+
+app.notFound((c) => {
+	return c.json({ error: "Not Found" }, 404);
+});
 
 app.get("/", (c) => {
 	return c.text("Hello Hono!");
 });
+
+app.get("/health", healthCheck);
 
 serve(
 	{
