65f3e8189dc728e4c89543262ca49519998c173b
avaice <dev@cho-ice.xyz>
2025-08-10 04:22:34 +0900

    dockerで起動できるようにした

--- carte/src/index.ts
+++ carte/src/index.ts
@@ -21,7 +21,7 @@ app.get("/health", healthCheck);
 serve(
 	{
 		fetch: app.fetch,
-		port: 3000,
+		port: 3002,
 	},
 	(info) => {
 		console.log(`Server is running on http://localhost:${info.port}`);
