9bf82a6fef7403820599c3afe09edf8dee7ade21
avaice <dev@cho-ice.xyz>
2025-08-17 20:49:18 +0900

    calcとdbの接続

--- packages/calc/src/index.ts
+++ packages/calc/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello")
+import prisma from 'database';
+
+async function main() {
+	// DBからOrganizationを1件取得して表示する例
+	const org = await prisma.organization.findFirst();
+	console.log('Organization:', org);
+}
+
+main().catch(console.error);
